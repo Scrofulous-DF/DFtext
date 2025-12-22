d36ae0f09207d9d8852582b3b927720183ab0019
Scrofulous-DF <kylerbicknell@gmail.com>
2025-12-22 17:50:15 +1300

    starting of the code

--- DFtext.cpp
+++ DFtext.cpp
@@ -1,6 +1,27 @@
 #include <iostream>
+#include <string>
+#include <fstream>
+#include <filesystem>
+
+using namespace std;
+
+filesystem::path filecd = "C:/Users/Kyleb/OneDrive/Desktop/Personal_Projects/Github/DFtext/.DFfiles/test1.df";
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    
+    if (filecd.extension() != ".df") {
+        cerr << "File is not .df" << endl;
+    }
+
+    fstream ProgramFile(filecd);
+
+    char ch;
+
+    while (ProgramFile.get(ch)) {
+        cout << ch;
+    }
+
+    ProgramFile.close();
+
     return 0;
 }
